40a31994991d41da31d850209746afad8ecb9b0b
flash-source <nehagoyal5557@gmail.com>
2026-01-08 21:38:33 +0530

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import './Navbar.css'
+
+export default Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="nav-logo">STRANGER THINGS</div>
+
+      <ul className="nav-links">
+        <li>Home</li>
+        <li>Characters</li>
+        <li>Episodes</li>
+        <li>Upside Down</li>
+      </ul>
+    </nav>
+  )
+}
+
